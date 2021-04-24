ab86a9ca3a2e9911df81fd7e32b38413776fc495
Mathews Jose <mathewsjose1998@gmail.com>
2021-04-24 16:45:36 -0500

    ADD:navbar

--- src/Nav.js
+++ src/Nav.js
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import "./Nav.css";
 
 function Nav() {
-  return <div></div>;
+  const [show, handleShow] = useState(false);
+  useEffect(() => {
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 150) {
+        handleShow(true);
+      } else {
+        handleShow(false);
+      }
+    });
+  }, []);
+
+  return (
+    <div className={`nav ${show && "nav_black"}`}>
+      <img
+        className="nav_logo"
+        src="https://pngimg.com/uploads/netflix/netflix_PNG26.png"
+        alt="logo"
+      />
+
+      <img
+        className="nav_avatar"
+        src="https://toppng.com/uploads/preview/hacker-avatar-11556286068ziooyvonc2.png "
+        alt="logo"
+      />
+    </div>
+  );
 }
 
 export default Nav;
